a0b48cef5818c93cb5e47056c77417f57f0de02b
Ad astra <155658018+ZyfNO2@users.noreply.github.com>
2026-07-22 23:39:01 +0800

    feat(router): export routing contracts

--- src/paperagent/providers/__init__.py
+++ src/paperagent/providers/__init__.py
@@ -1,9 +1,19 @@
 from paperagent.providers.base import FixtureKey, LLMProvider, SearchProvider
 from paperagent.providers.config import load_provider_config
+from paperagent.providers.endpoint import (
+    EndpointCapabilities,
+    EndpointConfig,
+    EndpointHealthState,
+    EndpointLimits,
+    EndpointProtocol,
+    ProviderPool,
+    RoutedEndpoint,
+)
 from paperagent.providers.fake_llm import FakeLLMProvider
 from paperagent.providers.fake_search import FakeSearchProvider, SearchFixtureKey
 from paperagent.providers.mistral import MistralLLMCall, MistralLLMProvider
 from paperagent.providers.openai_llm import OpenAILLMProvider
+from paperagent.providers.router import EndpointSnapshot, RouteAttempt, RoutingLLMProvider
 from paperagent.providers.runtime import (
     InvocationTelemetry,
     LLMProviderName,
@@ -17,6 +27,12 @@ from paperagent.providers.runtime import (
 from paperagent.providers.runtime_factory import build_llm_provider
 
 __all__ = [
+    "EndpointCapabilities",
+    "EndpointConfig",
+    "EndpointHealthState",
+    "EndpointLimits",
+    "EndpointProtocol",
+    "EndpointSnapshot",
     "FakeLLMProvider",
     "FakeSearchProvider",
     "FixtureKey",
@@ -28,7 +44,11 @@ __all__ = [
     "OpenAILLMProvider",
     "ProviderError",
     "ProviderErrorCode",
+    "ProviderPool",
     "ProviderRuntimeConfig",
+    "RouteAttempt",
+    "RoutedEndpoint",
+    "RoutingLLMProvider",
     "SearchFixtureKey",
     "SearchProvider",
     "TaskBudget",
